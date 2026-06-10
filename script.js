8dd511d36b2126c635b1d16185216759890cae76
JasonGlowing <91945799+JasonGlowing@users.noreply.github.com>
2026-06-10 11:57:06 +0300

    Перенос скриптов в script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var typed = new Typed('#element', {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-    typeSpeed: 50,
-})
+let strings = [
+    'Прогер',
+    'Музыкант',
+    'Пригожин Вагнер (мой одногруппник не хочет чтобы я это удалял)',
+    'Халяль'
+];
+
+let typed = new Typed('#typed', {
+    strings: strings,
+    typeSpeed: 20,
+    backSpeed: 20,
+    loop: true
+});
+
+function toggleMenu() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
